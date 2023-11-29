4e08e0163cab7b76ca87ce7c170ebf64c35b42f6
Adebisijoshua <Adekunle8k@gmail.com>
2023-11-28 16:36:55 -0800

    I am trying

--- 0x01-python-if_else_loops_functions/102-magic_calculation.py
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -1,8 +1,7 @@
+#!/usr/bin/python3
 # File name : 102-magic_calculation.py
 # Adekunle Joshua Adebisi Adekunle8k@gmail.com
 
-
-#!/usr/bin/python3
 def magic_calculation(a, b, c):
     if a < b:
         return (c)
