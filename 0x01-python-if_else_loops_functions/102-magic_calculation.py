01858474a61808a3ee97ef8b655a90cef8eac1cb
Adebisijoshua <Adekunle8k@gmail.com>
2023-11-28 16:35:25 -0800

    I am trying

--- 0x01-python-if_else_loops_functions/102-magic_calculation.py
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -1,12 +1,12 @@
-#!/usr/bin/python3
 # File name : 102-magic_calculation.py
 # Adekunle Joshua Adebisi Adekunle8k@gmail.com
 
 
+#!/usr/bin/python3
 def magic_calculation(a, b, c):
-    """Match bytecode provided by Holberton School."""
     if a < b:
         return (c)
-    if c > b:
+    elif c > b:
         return (a + b)
-    return (a*b - c)i
+    else:
+        return (a * b - c)
