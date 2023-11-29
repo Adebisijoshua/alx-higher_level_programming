5465631d8b2ac8205db67c511023e032b6b41d6e
Adebisijoshua <Adekunle8k@gmail.com>
2023-11-28 16:06:15 -0800

    I have stuck on this for a while

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -1,25 +1,42 @@
-#ifndef LISTS_H
-#define LISTS_H
-
 #include <stdlib.h>
-
+#include <stdio.h>
+#include "lists.h"
 /**
- * struct listint_s - singly linked list
- * @n: integer
- * @next: points to the next node
- *
- * Description: singly linked list node structure
- * for Holberton project
+ * insert_node - put node in mode to linkedlist
+ * @head: head
+ * @number: num that will be added
+ * Return: the address for the new code
  */
-typedef struct listint_s
+listint_t *insert_node(listint_t **head, int number)
 {
-	int n;
-	struct listint_s *next;
-} listint_t;
+	listint_t *new = malloc(sizeof(listint_t));
+	listint_t *actual = *head;
 
-size_t print_listint(const listint_t *h);
-listint_t *add_nodeint_end(listint_t **head, const int n);
-void free_listint(listint_t *head);
-listint_t *insert_node(listint_t **head, int number);
+	if (!new)
+		return (NULL);
 
-#endif /* LISTS_H */
+	new->n = number;
+
+	if (*head == NULL || (*head)->n > number)
+	{
+		new->next = *head;
+		*head = new;
+		return (new);
+	}
+
+	while (actual->next)
+	{
+		if ((actual->next)->n >= number)
+		{
+			new->next = actual->next;
+			actual->next = new;
+			return (new);
+		}
+		actual = actual->next;
+	}
+
+	new->next = NULL;
+	actual->next = new;
+
+	return (new);/* return */
+}
