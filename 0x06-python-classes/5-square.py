4b09567251b78d44f2f9709119347ab9618b2c5f
Adebisijoshua <Adekunle8k@gmail.com>
2023-12-19 17:37:30 -0800

    I am not giving up

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -17,8 +17,18 @@ class Square:
     @size.setter
     def size(self, value):
         """Setter method"""
-        self.__size = value
         if type(value) != int:
             raise TypeError("size must be an integer")
         if value < 0:
             raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def my_print(self):
+        """Prints the square"""
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(self.__size):
+                for j in range(self.__size):
+                    print('#', end='')
+                print()
