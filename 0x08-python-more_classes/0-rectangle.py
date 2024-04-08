0daf89a15442d6bc2aa52c58910d0cbe51f23147
Adebisijoshua <Adekunle8k@gmail.com>
2024-04-08 15:08:29 -0700

    I hope this code work after my long break

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""0-rectangle, built for Alx  project 0x08 task 0.
+"""
+
+
+class Rectangle:
+    """Empty class per task instructions, will be built on this  in the coming tasks.
+    """
+    pass
