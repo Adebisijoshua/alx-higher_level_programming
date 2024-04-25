b66a5de28a276c381d6139138ad01bfe85388d78
Adebisijoshua <Adekunle8k@gmail.com>
2024-04-25 16:18:48 -0400

    I just edited the file now

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,14 +1,9 @@
 #!/usr/bin/python3
-""" The 3-main """
-from models.rectangle import Rectangle
+"""
+This Contains  is_kind_of_class function
+"""
 
-if __name__ == "__main__":
 
-    r1 = Rectangle(3, 2)
-    print(r1.area())
-
-    r2 = Rectangle(2, 10)
-    print(r2.area())
-
-    r3 = Rectangle(8, 7, 0, 0, 12)
-    print(r3.area())
+def is_kind_of_class(obj, a_class):
+    """Yes if obj is an instance or inherited from a_class, else False"""
+    return (isinstance(obj, a_class))
