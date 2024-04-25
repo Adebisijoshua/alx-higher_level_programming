dc85799313f49cd5010f0d6bdbb4f8ad7d02d8d6
Adebisijoshua <Adekunle8k@gmail.com>
2024-04-25 16:36:17 -0400

    4-inherits_from.py

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,9 +1,9 @@
-i#!/usr/bin/python3
+#!/usr/bin/python3
 """
-Has the inherits_from function
+The inherits_from function
 """
 
 
 def inherits_from(obj, a_class):
-    """gives yes if obj is a subclass of a_class, otherwise false"""
-    return(issubclass(type(obj), a_class) and type(obj) != a_class)
+    """gives yes if obj is a subclass of a_class, or else No"""
+    return(issubclass(type(obj), a_class) and type(obj) != a_class)i
