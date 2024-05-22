06abf433df8fd50c0ef3bbf9abf3a5f657bdf7df
Adebisijoshua <Adekunle8k@gmail.com>
2024-05-21 21:04:44 -0700

    corrected file

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -4,7 +4,7 @@ CREATE table IF NOT EXISTS second_table (`id` INT,
 `score` INT);
 INSERT INTO `second_table` (`id`, `name`, `score`)
 VALUES
-       (1, 'Joshua', 10),
-       (2, 'JUdah', 3),
-       (3, 'Zin', 14),
+       (1, 'John', 10),
+       (2, 'Alex', 3),
+       (3, 'Bob', 14),
        (4, 'George', 8)
