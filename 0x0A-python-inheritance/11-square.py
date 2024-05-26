235129f97f461d6f4cec88d2772e7fecf5bb0e6a
Adebsijoshua <Adekunle8k@gmail.com>
2024-05-26 06:43:52 -0400

    a class Square that inherits from Rectangle (9-rectangle.py). (task based on 10-square.py)

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,14 +1,21 @@
 #!/usr/bin/python3
-"""the module gets a Rectangle subclass Square"""
+#Author name : Adekunle Joshua Adebisi
+"""11-square.py
+"""
+
+
 Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """present a square"""
+    """ inherits from Rectangle """
 
     def __init__(self, size):
-        """set a new square
-        """
+        """ Constructor """
         self.integer_validator("size", size)
-        super().__init__(size, size)
         self.__size = size
+        super().__init__(size, size)
+
+    def __str__(self):
+        """ print(self) method """
+        return '[Square] {:d}/{:d}'.format(self.__size, self.__size)
